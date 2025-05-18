086efea9f2f8a6b2b8d6705097ea6052d4630723
karimalisahili <kasahili.20@est.ucab.edu.ve>
2025-05-18 16:46:03 -0400

    a

--- client/code/main.py
+++ client/code/main.py
@@ -443,9 +443,7 @@ class Game:
                             self.trash_dict[t.id] = trash
                         else:
                             self.trash_dict[t.id].rect.topleft = (t.x, t.y)
-                    # Fix: avoid changing dict size during iteration
-                    tids_to_remove = [tid for tid in self.trash_dict.keys() if tid not in server_trash_ids]
-                    for tid in tids_to_remove:
+                    for tid in [tid for tid in list(self.trash_dict.keys()) if tid not in server_trash_ids]:
                         self.trash_dict[tid].kill()
                         del self.trash_dict[tid]
 
